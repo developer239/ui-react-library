3061e4ea503923c1c1d0ad86f08814e26780fb10
Michal Jarnot <m.jarnot@yahoo.com>
2019-04-26 16:12:52 +0200

    fix: failing build 💣

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin')
 
-module.exports = (baseConfig,
-  env,
-  config) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('awesome-typescript-loader'),
